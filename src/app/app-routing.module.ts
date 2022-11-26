dcf07a56198a93925a81064127cb42d6a52c2a5b
Sung <nosungju@gmail.com>
2022-11-26 13:24:35 -0300

    criando pagina cadastro cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { ClientesComponent } from './pages/clientes/clientes.component';
 import { ProdutosComponent } from './pages/produtos/produtos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'cadastro-produto', component: ProdutosComponent}
+  {path: 'cadastro-produto', component: ProdutosComponent},
+  {path: 'cadastro-cliente', component: ClientesComponent}
 ];
 
 @NgModule({
