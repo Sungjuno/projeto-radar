8ffa9bc3514e981d5f08daf96cfc4b28a343a368
Sung <nosungju@gmail.com>
2022-11-28 01:30:16 -0300

    implementando login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+    ) { }
+
+    loginUsuario = { login: 'teste', senha: '123'}
+
+    loginForm = this.fb.group({
+      login: (''),
+      senha:(''),
+    });
+
 
   ngOnInit(): void {
   }
+
+  verificaLogin(){
+    if(this.loginUsuario.login == this.loginForm.value.login && this.loginUsuario.senha == this.loginForm.value.senha){
+      this.auth.setLogin(this.loginForm.value.login)
+      this.router.navigate(['home'])
+     }
+    }
 
 }
